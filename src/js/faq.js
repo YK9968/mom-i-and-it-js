36ce23c8792a43c773647a82cb3137ee027fd7db
777Pavel777 <cross1295@icloud.com>
2024-03-16 13:00:28 +0200

    edits in  faq.js file

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,31 +1,43 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-new Accordion('.faq-column', {
-  duration: 400,
-  showMultiple: true,
-  onOpen: function (currentElement) {
-    console.log(currentElement);
-  },
-});
+document.addEventListener('DOMContentLoaded', accordeonOpenClose);
 
-document.addEventListener('DOMContentLoaded', function () {
-  const accButtons = document.querySelectorAll('.accordeon-button');
-
-  accButtons.forEach(function (button) {
-    button.addEventListener('click', function () {
-      const panel = this.parentElement.nextElementSibling;
-      const arrow = this.querySelector('.faq-arrow-img');
-
-      panel.classList.toggle('show');
-
-      if (panel.classList.contains('show')) {
-        arrow.style.transition = 'transform 0.5s';
-        arrow.style.transform = 'rotate(180deg)';
-      } else {
-        arrow.style.transition = 'transform 0.5s';
-        arrow.style.transform = 'rotate(0deg)';
-      }
-    });
+function accordeonOpenClose() {
+  new Accordion('.faq-column', {
+    duration: 200,
+    showMultiple: true,
+    onOpen: currentElement => {
+      const arrow = currentElement.querySelector('.faq-arrow-img');
+      arrow.style.transition = 'transform 0.5s';
+      arrow.style.transform = 'rotate(180deg)';
+    },
+    onClose: currentElement => {
+      const arrow = currentElement.querySelector('.faq-arrow-img');
+      arrow.style.transition = 'transform 0.5s';
+      arrow.style.transform = 'rotate(0deg)';
+    },
   });
-});
+}
+
+// function initializeAccordion() {
+//   const accButtons = document.querySelectorAll('.accordeon-button');
+
+//   accButtons.forEach(function (button) {
+//     button.addEventListener('click', toggleAccordionPane);
+//     function toggleAccordionPane() {
+//       const panel = this.parentElement.nextElementSibling;
+//       const arrow = this.querySelector('.faq-arrow-img');
+
+//       panel.classList.toggle('show');
+
+//       if (panel.classList.contains('show')) {
+//         arrow.style.transition = 'transform 0.5s';
+//         arrow.style.transform = 'rotate(180deg)';
+//       } else {
+//         arrow.style.transition = 'transform 0.5s';
+//         arrow.style.transform = 'rotate(0deg)';
+//       }
+//     }
+//   });
+// }
