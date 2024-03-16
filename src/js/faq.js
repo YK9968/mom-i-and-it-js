14ed1a2e81dee98fc172285a145c3b792458c8a7
777Pavel777 <cross1295@icloud.com>
2024-03-16 14:17:47 +0200

    edits in faq.js

--- src/js/faq.js
+++ src/js/faq.js
@@ -5,39 +5,17 @@ document.addEventListener('DOMContentLoaded', accordeonOpenClose);
 
 function accordeonOpenClose() {
   new Accordion('.faq-column', {
-    duration: 200,
+    duration: 500,
     showMultiple: true,
-    onOpen: currentElement => {
+    beforeOpen: currentElement => {
       const arrow = currentElement.querySelector('.faq-arrow-img');
       arrow.style.transition = 'transform 0.5s';
       arrow.style.transform = 'rotate(180deg)';
     },
-    onClose: currentElement => {
+    beforeClose: currentElement => {
       const arrow = currentElement.querySelector('.faq-arrow-img');
       arrow.style.transition = 'transform 0.5s';
       arrow.style.transform = 'rotate(0deg)';
     },
   });
 }
-
-// function initializeAccordion() {
-//   const accButtons = document.querySelectorAll('.accordeon-button');
-
-//   accButtons.forEach(function (button) {
-//     button.addEventListener('click', toggleAccordionPane);
-//     function toggleAccordionPane() {
-//       const panel = this.parentElement.nextElementSibling;
-//       const arrow = this.querySelector('.faq-arrow-img');
-
-//       panel.classList.toggle('show');
-
-//       if (panel.classList.contains('show')) {
-//         arrow.style.transition = 'transform 0.5s';
-//         arrow.style.transform = 'rotate(180deg)';
-//       } else {
-//         arrow.style.transition = 'transform 0.5s';
-//         arrow.style.transform = 'rotate(0deg)';
-//       }
-//     }
-//   });
-// }
