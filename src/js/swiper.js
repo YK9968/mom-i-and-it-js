c2eed36a1f0152be58febcfcf030bc370f328ec2
Serhii-Yakimtso <serafyso@gmail.com>
2024-03-17 21:55:26 +0200

    change project btn classes like review btn

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,6 +1,34 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
-export default function getSlider(element, props) {
-  const slider = new Swiper(element, props);
-}
+// ===== slider for reviews section =====
+
+const reviewsSliderElement = document.querySelector('.reviews-swiper');
+const reviewsSliderProps = {
+  navigation: {
+    nextEl: '.reviews-btn-next',
+    prevEl: '.reviews-btn-prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    // when window width is >= 1440px
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 16,
+    },
+  },
+};
+
+const reviewsSlider = new Swiper(reviewsSliderElement, reviewsSliderProps);
