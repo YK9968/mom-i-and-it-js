439ee7758d7c9c23ff82e4c0337f85e4aa65990d
SergeyPanchenko72 <143348555+SergeyPanchenko72@users.noreply.github.com>
2024-03-17 17:12:19 +0200

    Update covers.js

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,25 +1,3 @@
-// function controlsTheAnimation() {
-//   const coversGallery = document.querySelector('.covers-content');
-//   const coversAnimation = document.querySelectorAll('.covers-marquee-line');
-//   const rect = coversGallery.getBoundingClientRect();
-//   const viewportHeight = window.innerHeight;
-//   const visibleHeight =
-//     Math.min(rect.bottom, viewportHeight) - Math.max(rect.top, 0);
-//   if (visibleHeight / viewportHeight >= 0.2) {
-//     if (!coversAnimation.classList.contains('animation')) {
-//       coversAnimation.classList.add('animation');
-//       console.log(coversAnimation.classList);
-//     }
-//     coversAnimation.style.animationPlayState = 'running';
-//   } else {
-//     if (coversAnimation.classList.contains('animation')) {
-//       coversAnimation.classList.remove('animation');
-//       console.log(coversAnimation.classList);
-//     }
-//     coversAnimation.style.animationPlayState = 'paused';
-//   }
-// }
-// window.addEventListener('scroll', controlsTheAnimation);
 function controlsTheAnimation() {
   const coversGalleries = document.querySelectorAll('.covers-content');
   coversGalleries.forEach(coversGallery => {
