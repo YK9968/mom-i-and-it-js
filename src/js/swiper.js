051b1364fc04b77b4927ee91dfa811ede9dfd2f8
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2024-03-18 16:17:48 +0200

    about-swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -43,3 +43,52 @@ const reviewsSliderProps = {
 };
 
 const reviewsSlider = new Swiper(reviewsSliderElement, reviewsSliderProps);
+
+//*ABOUT*//
+
+const aboutMeElement = document.querySelector('.about-swiper');
+const aboutMeProps = {
+  modules: [Navigation, Keyboard],
+  slidesPerView: 2,
+  watchOverflow: true,
+  speed: 300,
+  navigation: {
+    nextEl: '.about-swiper-button-next',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+    },
+    768: {
+      slidesPerView: 4,
+      slidesPerGroup: 1,
+    },
+    1440: {
+      slidesPerView: 6,
+      slidesPerGroup: 1,
+    },
+  },
+};
+
+const aboutMeSlider = new Swiper(aboutMeElement, aboutMeProps);
+
+const swiperBtn = document.querySelector('.about-swiper-btn');
+
+swiperBtn.addEventListener('click', () => {
+  const activeSlide = document.querySelector('.about-swiper-slide.active');
+  const nextSlide = activeSlide.nextElementSibling;
+
+  if (nextSlide) {
+    activeSlide.classList.remove('active');
+    nextSlide.classList.add('active');
+  } else {
+    const firstSlide = document.querySelector('.swiper-slide');
+    activeSlide.classList.remove('active');
+    firstSlide.classList.add('active');
+  }
+});
