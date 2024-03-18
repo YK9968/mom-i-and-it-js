71028d0d98681949503b00d5b16dbabc02ab5014
DaryPet <d7akkord@gmail.com>
2024-03-18 13:40:07 +0100

    modal positive&negative-css+js

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import './js/about-accordion.js';
 import './js/faq.js';
 import './js/reviews-fuctions.js';
 import './js/covers.js';
-import'./js/header-modal-window.js';
+import './js/header-modal-window.js';
 import './js/swiper.js';
+import './js/modal.js';
