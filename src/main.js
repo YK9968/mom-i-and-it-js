42cd8939ad51c95213b8e3334eef50129131e4a5
yurii <robka4an@gmail.com>
2024-03-19 15:47:39 +0200

    fanaly update ( maybe )

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
+import './js/header-modal-window.js';
+import './js/accordion.js';
+import './js/swiper.js';
 import './js/reviews-fuctions.js';
 import './js/covers.js';
-import './js/header-modal-window.js';
-import './js/swiper.js';
-import './js/accordion.js';
 import './js/modal.js';
 import './js/form-post.js';
